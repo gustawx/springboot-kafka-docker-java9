27658015fbbbaa538aac083122d752159bcc064a
Sebastian Kustosz <skustosz@gmail.com>
2017-12-22 17:37:34 +0100

    unit test update

--- src/test/java/unit/MoviesControllerTest.java
+++ src/test/java/unit/MoviesControllerTest.java
@@ -3,38 +3,43 @@ package unit;
 import app.App;
 import app.controller.HomeController;
 import app.model.Movie;
+import app.service.MovieParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.BDDMockito.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
-@RunWith(SpringRunner.class)
+@RunWith(PowerMockRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = App.class)
 public class MoviesControllerTest {
 
-    @MockBean
-    private HomeController homeController;
-
+    @PrepareForTest(MovieParser.class)
     @Test
     public void getMoviesTest(){
-        given(this.homeController.home()).willReturn(Set.of(
-                    new Movie("Fake title", 1982),
-                    new Movie("Another Fake title", 2007))
-                );
+        mockStatic(MovieParser.class);
+        PowerMockito.when(MovieParser.getMovies()).thenReturn(Set.of(
+                new Movie("Fake title", 1982),
+                new Movie("Another Fake title", 2007)));
 
-        Set<Movie> m = homeController.home();
+        HomeController hc = new HomeController();
+
+        Set<Movie> m = hc.home();
         Set<Movie> mockM = (Set.of(
                 new Movie("Fake title", 1982),
                 new Movie("Another Fake title", 2007)));
 
-        System.out.println(mockM.equals(m));
         assertThat(m, equalTo(mockM));
+
+        assertThat(m, hasItems(new Movie("Fake title", 1982),
+                new Movie("Another Fake title", 2007)));
     }
 }
