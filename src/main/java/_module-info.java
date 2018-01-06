83cbc60b001c8a1e6cd97e6af78dc76ecd9febec
Sebastian Kustosz <skustosz@gmail.com>
2018-01-06 14:09:00 +0100

    module-info.java added

---
+++ src/main/java/_module-info.java
@@ -0,0 +1,17 @@
+/*
+open module app {
+    requires java.xml.bind;
+    requires javax.transaction.api;
+    requires hibernate.jpa;
+    requires spring.beans;
+    requires spring.core;
+    requires spring.context;
+    requires spring.tx;
+    requires spring.web;
+    requires spring.webmvc;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+}
+*/
